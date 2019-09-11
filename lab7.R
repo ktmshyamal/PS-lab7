72785f180cd9d6c931c5a27264ba903442267f84
Hewagama K.G. it18165180 <it18165180@my.sliit.lk>
2019-09-11 15:50:27 +0530

    Basic is done..!

---
+++ lab7.R
@@ -0,0 +1,8 @@
+data<-read.table("C:/Users/IT18165180/Desktop/PS-lab7/Forest.txt",header = TRUE,sep = ",")
+#to read all the data from the txt file
+
+attach(data)
+#to analys the data
+
+View(data)
+#to view the data
